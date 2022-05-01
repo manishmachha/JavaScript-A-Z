f51aaabb5f8303c96dc316f57dedeaaa3119a558
Manish Machha <manishmachha@gmail.com>
2022-05-01 14:30:03 +0530

    v2

--- part1/70.js
+++ part1/70.js
@@ -1,11 +1,11 @@
 // optional chaining 
 
-const user  = {
+const user = {
     firstName: "harshit",
     // address: {houseNumber: '1234'}
 }
 
 
 
-console.log(user?.firstName);
-console.log(user?.address?.houseNumber);
+console.log(user ? .firstName);
+console.log(user ? .address ? .houseNumber);
